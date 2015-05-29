967be696608eda406d4150a955c0ad7671aa7654
Guillaume Perrin <gperrin01@gmail.com>
2015-05-29 11:42:57 +0100

    ok to query by artist and render all artists and their picture

--- js/spotify.js
+++ js/spotify.js
@@ -8,27 +8,30 @@
 // ON click on search-type, nthign yet, store the type
 // ON click submit, get info form spotify and put on results
 $(document).ready(function() {
-  $(":submit").on('click', getResults)
+  $("#go").on('click', getResults)
 });
 
-var type;
-var content;
+// var type;
+// var content;
 
 function getResults(e) {
-  type = $('#search-type').val();
-  content = $('#search-keyword').val();
-  // $.ajax({
-  //   type: 'GET',
-  //   url: "http://ws.spotify.com/search/1/track.json?q="+content
-  // }).done(function(response) {
-  //   console.log(response);
+  e.preventDefault();
+  var storage = [];
+  var type = $('#search-type').val();
+  var content = $('#search-keyword').val();
   $.ajax({
-   type: "GET",
-   url: 'https://api.spotify.com/v1/search?q=coldplay&type=track'
- }).done(function(data, response){
-   console.log(response);
+    type: 'GET',
+    url: "https://api.spotify.com/v1/search?q="+content+"&type="+type
+  }).done(function(response) {
+    // debugger;
+    $.each(response.artists.items, function(index, artist) {
+      if ($.inArray(artist.name, storage) == -1) {
+        storage.push(artist.name);
+        var img = artist.images.length > 0 ? artist.images[0].url : 'img/no_image.gif'
+        $('#results').append("<div>  <p><a href="+artist.href +">"+ artist.name +"</a></p> <img src=" + img +">  </div>"   );
+      }
+    })
   })
-
 }
 
 
